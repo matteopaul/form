7b7a3499e5263cc12e77b125b43068ace4cf3a6c
matteopaul <matteo.illing@icloud.com>
2018-11-07 13:38:07 +0100

    Tab 4 hinzugefügt, inkl. Microanimations + validation

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,6 +2,8 @@ var progressBarPos = 0;
 var chooseButtons1 = document.getElementsByClassName('tab1--selection-item-select');
 var chooseButtons2 = document.getElementsByClassName('tab2--selection-item-select');
 var chooseButtons3 = document.getElementsByClassName('tab3--selection-item');
+var titleButtons = document.getElementsByClassName('tab4--form-select-option');
+var inputs = document.getElementById('tab4--form-container').getElementsByTagName('input');
 var currentTab = 0;
 var confirmedData = [];
 var tabs = document.getElementsByClassName('tab');
@@ -31,6 +33,27 @@ function changeTab(step) {
   } else {
     moveProgressBar(19,5);
   }
+}
+
+function validateForm() {
+  for(var i = 0; i < inputs.length; i++) {
+    if(inputs[i].value !== "") {
+      if(i == inputs.length - 1) {
+        if(document.getElementById('tab4--form-name-show').innerHTML !== "Title") {
+          console.log('show');
+          return true;
+        } else {
+          console.log('hide wegen title');
+        }
+      } else {
+        console.log('hide weil es nicht das letzte item ist');
+      }
+    } else {
+      break;
+      console.log('hide weil das item leer ist');
+    }
+  }
+  return false;
 }
 
 if(window.addEventListener) {
@@ -81,4 +104,33 @@ document.getElementById('tab3--selection-select').addEventListener('click', func
       confirmedData.push(document.getElementsByClassName('active')[0].id);
       changeTab(1);
     }
+});
+
+document.getElementById('tab4--form-name-show').addEventListener('click', function() {
+    this.parentElement.getElementsByClassName('tab4--form-select-horizontal')[0].classList.toggle('show');
+});
+
+for(var i = 0; i < titleButtons.length; i++) {
+  titleButtons[i].addEventListener('click', function() {
+    document.getElementById('tab4--form-name-show').innerHTML = this.innerHTML;
+    this.parentElement.classList.toggle('show');
+    validateForm();
+  });
+}
+
+for(var i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener('keypress', function(){
+    if(validateForm() == true) {
+      document.getElementById('tab4--selection-select').style.visibility = 'visible';
+    } else {
+      document.getElementById('tab4--selection-select').style.visibility = 'hidden';
+    }
+  });
+}
+
+document.getElementById('tab4--selection-select').addEventListener('click', function() {
+  var participant = [document.getElementById('tab4--form-name-show').innerHTML, document.getElementById('tab4--form-name').value, document.getElementById('tab4--form-email').value, document.getElementById('tab4--form-street').value,document.getElementById('tab4--form-state').value,document.getElementById('tab4--form-postcode').value,document.getElementById('tab4--form-participants').value];
+  confirmedData.push(participant);
+  changeTab(1);
+  console.log(confirmedData);
 });
